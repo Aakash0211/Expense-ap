54093427b09a6a0ef7cad5dff2de3a01fa2ba088
Aakash <akashkushi111@gmail.com>
2020-10-02 11:11:09 +0530

    Updates

---
+++ precache-manifest.f5b3f68af0c917def4169bb49109a0ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4b881e16b92fa59c29d8af988e47b5",
+    "url": "/expense-ap/index.html"
+  },
+  {
+    "revision": "fe4c7d446925432a94a1",
+    "url": "/expense-ap/static/css/main.06bdb4db.chunk.css"
+  },
+  {
+    "revision": "c151fcf8a54f332ce083",
+    "url": "/expense-ap/static/js/2.19d3742b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-ap/static/js/2.19d3742b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4c7d446925432a94a1",
+    "url": "/expense-ap/static/js/main.07de3214.chunk.js"
+  },
+  {
+    "revision": "afbf7d7d383c0c53d8cb",
+    "url": "/expense-ap/static/js/runtime-main.7ed1b005.js"
+  }
+]);
